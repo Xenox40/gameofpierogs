3784bcce79c6399e1023a29b639ccdd5d552b7ad
Michał Łuszczyk <michal.luszczyk@gmail.com>
2014-06-04 15:41:48 +0200

    removed a leak in memory…

--- world.cpp
+++ world.cpp
@@ -12,18 +12,19 @@ World::World()
 void World::initSample()
 {
     QString t = "Secret room hiding %1";
-    Room a(t.arg("cucumbers")), b(t.arg("tomtatoes")),
-        c(t.arg("potatoes"));
+    Room *a = new Room(t.arg("cucumbers"));
+    Room *b = new Room(t.arg("tomatoes"));
+    Room *c = new Room(t.arg("potatoes"));
 
-    a.bind(&b, Room::EAST);
-    b.bind(&c, Room::SOUTH);
+    a->bind(b, Room::EAST);
+    b->bind(c, Room::SOUTH);
 
-    hero = new Knight(&a);
+    hero = new Knight(a);
     Dragon* d = new Dragon[3];
 
-    a.addCharacter(&d[0]);
-    c.addCharacter(&d[1]);
-    c.addCharacter(&d[2]);
+    a->addCharacter(&d[0]);
+    c->addCharacter(&d[1]);
+    c->addCharacter(&d[2]);
 }
 
 const Room* World::currentRoom() const
